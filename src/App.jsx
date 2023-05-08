3f1d28cff457d23d39c7f82cf9c82b63c4369ef1
padilha <igor.padilha2004@gmail.com>
2023-05-07 21:10:00 -0300

    🎉 inital project structure

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import Task from './task/Task'
+
+function App() {
+  return (
+    <Task title="Task to do" description="test"/>
+  )
+}
+
+export default App
---
+++ src/task/Task.jsx
@@ -0,0 +1,12 @@
+import './task.css'
+
+function Task({ title, description }) {
+    return (
+        <div className="task">
+            <h3 className="title">{title}</h3>
+            <p>{description}</p>
+        </div>
+    )
+}
+
+export default Task;
