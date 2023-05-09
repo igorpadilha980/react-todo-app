47db249975bc3c72c4b48f978bde17c71f97bce8
padilha <igor.padilha2004@gmail.com>
2023-05-09 18:22:56 -0300

    ✨ create TaskListItem component

--- src/task/TaskList.jsx
+++ src/task/TaskList.jsx
@@ -1,11 +1,22 @@
 import Task from "./Task"
 
+import './task-list.css'
+
+function TaskListItem({ task }) {
+    return (
+        <li key={task.id} className="item">
+            <input type="checkbox" checked={task.completed} />
+            <Task {...task}/>    
+        </li>
+    )
+}
+
 function TaskList({tasks}) {
     const taskItems = []
 
     tasks
-        .map(t => Task({key: t.id, ...t}))
-        .forEach(task => taskItems.push(task))
+        .map(task => TaskListItem({ task }))
+        .forEach(taskItem => taskItems.push(taskItem))
 
     return (
         <ul className="task-list">
