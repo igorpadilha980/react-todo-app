89a9114d15bbe9c7c621943bc2b9d673d2673146
padilha <igor.padilha2004@gmail.com>
2023-05-24 00:11:45 -0300

    ✨ make Navbar aware of user auth state

--- src/page/Navbar.jsx
+++ src/page/Navbar.jsx
@@ -1,9 +1,20 @@
+import { Link } from 'react-router-dom';
+import { useAuth } from '../auth/AuthContext'
+
 import './navbar.css'
 
 function Navbar() {
+    const { isSigned, signOut } = useAuth()
+
     return (
         <nav className='navbar'>
             Todo
+
+            {
+                isSigned() ?
+                <button onClick={signOut}>Logout</button> :
+                <Link to="/login">Login</Link>
+            }
         </nav>
     )
 }
