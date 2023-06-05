12db851450a5f89656410a1304c9597c13975ef8
padilha <igor.padilha2004@gmail.com>
2023-06-05 00:31:41 -0300

    ✅ create unit tests for task service

---
+++ test/task.test.js
@@ -0,0 +1,65 @@
+import { initializeTestEnvironment } from '@firebase/rules-unit-testing'
+import { addDoc, collection, doc, getDoc, getDocs } from 'firebase/firestore'
+import { afterAll, afterEach, assert, describe, expect, test, vi } from 'vitest'
+
+const testEnv = await initializeTestEnvironment({ projectId: 'todo-app' })
+
+describe('task service suite', async () => {
+    const testUserId = 'test-user'
+    const testTaskData = {
+        title: 'Test task',
+        description: 'Test task description'
+    }
+
+    const app = testEnv.authenticatedContext(testUserId)
+    const firestore = app.firestore()
+
+    vi.doMock('../src/firebase/firebase', () => {
+        return {
+            firestore
+        }
+    })
+
+    afterEach(async () => {
+        await testEnv.clearFirestore()
+    })
+
+    const { default: taskService } = await import('../src/task/service')
+
+    test('if creates add task to user', async () => {
+        const savedTask = await taskService.createTask(testUserId, testTaskData);
+        expect(savedTask).toMatchObject({
+            id: expect.any(String),
+            completed: expect.any(Boolean),
+            ...testTaskData
+        })
+
+        const listRequest = getDocs(collection(firestore, 'users', testUserId, 'tasks')).then(snap => snap.docs)
+        await expect(listRequest).resolves.toHaveLength(1)
+    })
+
+    test('if can fetch tasks from user', async () => {
+        await taskService.createTask(testUserId, testTaskData)
+
+        const taskList = await taskService.allTasks(testUserId)
+        expect(taskList).toHaveLength(1)
+    })
+
+    test('if updates task status', async () => {
+        const task = await taskService.createTask(testUserId, testTaskData)
+        const newStatus = true
+
+        await taskService.updateTaskStatus(testUserId, task.id, newStatus)
+
+        const updatedTask = await getDoc(doc(firestore, 'users', testUserId, 'tasks', task.id)).then(doc => doc.data())
+        assert(updatedTask.completed == newStatus)
+    })
+
+    test('if deletes task from user', async () => {
+        const task = await taskService.createTask(testUserId, testTaskData)
+        await taskService.deleteTask(testUserId, task.id)
+
+        const userTasks = await taskService.allTasks(testUserId);
+        expect(userTasks).toHaveLength(0)
+    })
+})
