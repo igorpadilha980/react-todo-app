b73fbe89814e1551bf264766914d9a0bae8014ca
padilha <igor.padilha2004@gmail.com>
2023-06-04 22:55:41 -0300

    👷 create workflows pipeline

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const urlBase = globalThis.process.env.DEPLOY_BASE_URL
+
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: urlBase,
   plugins: [react()],
   css: {
     modules: {
