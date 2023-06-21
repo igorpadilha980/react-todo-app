04edfb36333618c99f4d28752048bd0665c1eb38
padilha <igor.padilha2004@gmail.com>
2023-06-21 16:03:20 -0300

    ✨ create watchAuthChange

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,4 +1,4 @@
-import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from 'firebase/auth'
+import { createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword, signOut } from 'firebase/auth'
 import { auth } from './firebase.js'
 import { createUser, fetchUser } from './user.js'
 
@@ -16,8 +16,18 @@ async function registerUser(username, email, password) {
         .then((credential) => createUser(credential.user.uid, username, email))
 }
 
+function watchAuthChange(callback) {
+    return onAuthStateChanged(auth, (user) => {
+        if (user)
+            fetchUser(user.uid).then(callback)
+
+        console.log({ user })
+    })
+}
+
 export {
     login,
     logout,
-    registerUser
+    registerUser,
+    watchAuthChange
 }
