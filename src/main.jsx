46f8bbc3890ed374a9fd00241285d68f5fb42761
padilha <igor.padilha2004@gmail.com>
2023-06-22 23:59:24 -0300

    🩹 fix direct url access resulting in 404

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './pages/App.jsx'
 import LoginPage from './pages/LoginPage.jsx'
-import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import { RouterProvider, createHashRouter } from 'react-router-dom'
 
 import './index.css'
 import { AuthProvider } from './contexts/AuthContext.jsx'
 import { SignUpPage } from './pages/SignUpPage.jsx'
 
-const router = createBrowserRouter([
+const router = createHashRouter([
   {
     index: true,
     element: <App />
