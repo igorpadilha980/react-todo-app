fa0944e40d1bfd83517d9e10a1fe9817575bf62c
padilha <igor.padilha2004@gmail.com>
2023-06-27 20:05:01 -0300

    ✨ add edit button to TaskList

--- src/components/TaskList/TaskList.jsx
+++ src/components/TaskList/TaskList.jsx
@@ -1,10 +1,11 @@
 import TaskCard from "../TaskCard"
 
 import DeleteIcon from '@mui/icons-material/DeleteSharp';
+import EditIcon from '@mui/icons-material/EditSharp';
 
 import './task-list.css'
 
-function TaskListItem({ task, onStatusUpdate, onRemove }) {
+function TaskListItem({ task, onStatusUpdate, onRemove, onEdit }) {
 
     const handleStatusChange = (changeEvent) => {
         const newStatus = changeEvent.target.checked
@@ -14,31 +15,35 @@ function TaskListItem({ task, onStatusUpdate, onRemove }) {
 
     const handleDeleteClick = () => onRemove && onRemove(task.id)
 
+    const handleEditClick = () => onEdit && onEdit(task)
+
     return (
         <li key={task.id} className="item">
             <input type="checkbox" checked={task.completed} onChange={handleStatusChange} className="status"/>
             <TaskCard {...task} className="task"/>
             <div className="options">
+                <button onClick={handleEditClick}><EditIcon /></button>    
                 <button onClick={handleDeleteClick}><DeleteIcon /></button>    
             </div>
         </li>
     )
 }
 
-function TaskList({ tasks, updateTaskStatus, onRemoveTask }) {
+function TaskList({ tasks, updateTaskStatus, onRemoveTask, onEditTask }) {
     const taskItems = []
 
     tasks
         .map(task => TaskListItem({
             task,
             onStatusUpdate: updateTaskStatus,
-            onRemove: onRemoveTask
+            onRemove: onRemoveTask,
+            onEdit: onEditTask
         }))
         .forEach(taskItem => taskItems.push(taskItem))
 
     return (
         <ul className="task-list">
-            {...taskItems}
+            {taskItems}
         </ul>
     )
 }
